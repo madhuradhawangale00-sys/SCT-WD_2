630873b98e1a80092a9475df6ed02763318bcf1f
madhuradhawangale00-sys <madhuradhawangale00-sys@email.com>
2026-06-13 12:34:55 +0530

    javascript adding

---
+++ script.js
@@ -0,0 +1,11 @@
+let startTime = 0;
+let elapsedTime = 0;
+let timerInterval = 0;
+let lapCount = 0;
+
+const display = document.getElementById('time-display');
+const startBtn = document.getElementById('start-btn');
+const pauseBtn = document.getElementById('pause-btn');
+const lapBtn = document.getElementById('lap-btn');
+const resetBtn = document.getElementById('reset-btn');
+const lapsList = document.getElementById('laps-list');
