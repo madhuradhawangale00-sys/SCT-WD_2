d4158b2dbb363e812e5e8f9a340d7a83e30b72df
madhuradhawangale00-sys <madhuradhawangale00-sys@email.com>
2026-06-13 12:36:13 +0530

    convert time to ms

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ const pauseBtn = document.getElementById('pause-btn');
 const lapBtn = document.getElementById('lap-btn');
 const resetBtn = document.getElementById('reset-btn');
 const lapsList = document.getElementById('laps-list');
+
+// Format time from milliseconds to HH:MM:SS.mm
+function formatTime(ms) {
+    let hrs = Math.floor(ms / 3600000);
+    let mins = Math.floor((ms % 3600000) / 60000);
+    let secs = Math.floor((ms % 60000) / 1000);
+    let msecs = Math.floor((ms % 1000) / 10);
+
+    return (
+        (hrs < 10 ? "0" + hrs : hrs) + ":" +
+        (mins < 10 ? "0" + mins : mins) + ":" +
+        (secs < 10 ? "0" + secs : secs) + "." +
+        (msecs < 10 ? "0" + msecs : msecs)
+    );
+}
